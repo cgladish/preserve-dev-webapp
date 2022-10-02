6c2d404cebd83eca061575188171467b38e0d41e
Chase Gladish <chase.gladish@gmail.com>
2022-10-02 13:29:56 -0700

    Add withheader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import Layout from "../components/layout";
 
 export default function Home() {
-  return <Layout title="Preserve.dev">Howdy 🤠</Layout>;
+  return (
+    <Layout title="Preserve.dev" withHeader>
+      Howdy 🤠
+    </Layout>
+  );
 }
